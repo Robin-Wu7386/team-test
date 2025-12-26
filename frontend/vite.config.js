bbd912bee161fc4af98bdfe8544ed89fdffc7fe8
Robin-Wu7386 <ariwsx@gmail.com>
2025-12-26 13:46:47 +0800

    武荣彬主页面知识图谱合并

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+﻿import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
   server: {
     proxy: {
       "/chat": "http://127.0.0.1:8000",
       "/static": "http://127.0.0.1:8000"
     }
   }
-});
+})
